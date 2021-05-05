39f638ea92fd09e832e1138d5e242655fff41b60
Johan How <johanhow@gmail.com>
2021-05-05 14:43:07 -0700

    add material-ui, add header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,92 @@
-import logo from './logo.svg';
+import logo from './logo-symbol.svg';
 import './App.css';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import { AppBar, InputBase, Toolbar } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    backgroundColor: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      Hello
+      <AppBar position="static">
+        <Toolbar style={{ justifyContent: "space-between", backgroundColor: "#24292e" }}>
+          <img style={{ maxHeight: '50px' }} src={logo} alt="helpfulhuman_logo" />
+          <InputBase
+            style={{ backgroundColor: 'white' }}
+            placeholder="Search"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </Toolbar>
+
+
+      </AppBar>
     </div>
   );
 }
